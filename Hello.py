01922cdc5cc303eb70e81233c7663e7e0e5936c9
muteStoic <50288669+muteStoic@users.noreply.github.com>
2024-04-30 16:06:15 +0000

    12

--- Hello.py
+++ Hello.py
@@ -4,17 +4,6 @@ import time
 import random
 
 
-st.write("test")
-st.write("test2")
-st.write("test5")
-
-
-
-import streamlit as st
-from openai import OpenAI
-import time
-import random
-
 
 client = OpenAI()
 
