d722b6159d8caf1d8ed806977832a474a565bc39
muteStoic <50288669+muteStoic@users.noreply.github.com>
2024-05-05 16:24:49 +0000

    rqw

--- pages/2_new page.py
+++ pages/2_new page.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 st.write("test")
+st.write("test2")
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
