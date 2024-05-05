70c056cd2aa4a6bb1192d0646a76e452a459878a
muteStoic <50288669+muteStoic@users.noreply.github.com>
2024-05-05 16:04:08 +0000

    working calling from database

--- pages/2_new page.py
+++ pages/2_new page.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
+st.write("test")
+
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+st.dataframe(df)
