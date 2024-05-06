9104f8bbcc231f8f44244702ec09cc0639af9329
muteStoic <50288669+muteStoic@users.noreply.github.com>
2024-05-06 17:40:47 +0000

    d

--- pages/2_log in test.py
+++ pages/2_log in test.py
@@ -3,30 +3,29 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 
 st.write("test")
-st.write("test2")
-st.write("test33")
-st.write("test4")
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
-#df2 = conn.read(worksheet = "Sheet3")
-singlecell = conn.read(
+userDb = conn.read(
     spreadsheet ="https://docs.google.com/spreadsheets/d/1QIbtWBhbykxaEGtE3bb_vFz5yGnyiUF6Lll1S_IdhCc/edit?usp=sharing")
 
-#dff = pd.DataFrame({singlecell})
-dff = singlecell.at[2,"name"]
-st.dataframe(singlecell)
+dff = userDb.at[2,"name"]
 
-#id_loc = dff.at[3,"name"]
+
+
+
+
+
+
+
+st.dataframe(userDb)
 
 st.write(dff)
-
-
-
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
-
 st.dataframe(df)
-st.write(singlecell)
+
+userid = st.text_input("idLabel")
+
+
+
