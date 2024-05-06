9fcd352bc159e0e0773acf813f46e23253d7f557
muteStoic <50288669+muteStoic@users.noreply.github.com>
2024-05-06 16:53:23 +0000

    d

--- pages/3_new page test dataframe.py
+++ pages/3_new page test dataframe.py
@@ -4,7 +4,7 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+df = conn.read(worksheet="Sheet1")
 
 # Print results.
 for row in df.itertuples():
