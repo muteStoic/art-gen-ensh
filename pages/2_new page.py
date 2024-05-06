837b12f1eb473850ca941970221db13b9533b4ba
muteStoic <50288669+muteStoic@users.noreply.github.com>
2024-05-06 17:34:32 +0000

    s

--- pages/2_new page.py
+++ pages/2_new page.py
@@ -13,7 +13,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
 #df2 = conn.read(worksheet = "Sheet3")
 singlecell = conn.read(
-    spreadsheet ="https://docs.google.com/spreadsheets/d/1Y5oaJN_XDAy6iM7yVAKKqIofI1WICOmYKWCD3OVQ8E0/edit?usp=sharing")
+    spreadsheet ="https://docs.google.com/spreadsheets/d/1QIbtWBhbykxaEGtE3bb_vFz5yGnyiUF6Lll1S_IdhCc/edit?usp=sharing")
 
 #dff = pd.DataFrame({singlecell})
 dff = singlecell.at[3,"name"]
